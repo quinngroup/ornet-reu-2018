cb1c4bb4947b67a4e67879ca3de9aff7522bdb42
Mojtaba Fazli <mojtaba@Mojtabas-iMac.local>
2018-06-30 21:48:39 -0400

    Issue 5 is done ...

--- src/tests.py
+++ src/tests.py
@@ -1,8 +1,9 @@
 import unittest
-
 import numpy as np
-
+import sys
+sys.path.insert(0, '/Users/mojtaba/Downloads/ornet-reu-2018-master-2/src')
 import raster_scan2 as raster_scan
+import read_video
 
 class RasterTest(unittest.TestCase):
 
@@ -20,8 +21,24 @@ class RasterTest(unittest.TestCase):
         return raster
 
     def test_rasterscan1(self):
+        
         y = np.arange(18).reshape((3, 3, 2))
         yt = self.manual_scan(y)
         yp = raster_scan.raster_scan(y)
 
         np.testing.assert_array_equal(yp, yt)
+
+    
+    def test_rasterscan_real(self):
+        
+        y = read_video.read_video('/Users/mojtaba/Desktop/OrNet Project/DAT VIDEOS/LLO/DsRed2-HeLa_2_21_LLO_Cell0.mov')
+        y = np.array(y[1:])
+        # Because of the output format of "read_video" module, we need to slice the "y"
+        y = y[0,:,:,:]
+        yt = self.manual_scan(y)
+        yp = raster_scan.raster_scan(y)
+        
+        np.testing.assert_array_equal(yp, yt)
+
+if __name__ == '__main__':
+    unittest.main()
